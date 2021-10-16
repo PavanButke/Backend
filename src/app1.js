114ea3e5b46372ff75cd235159f1304cef5aedd7
PavanButke <butkepavan55@gmail.com>
2021-10-16 18:05:13 +0530

    ready

--- src/app1.js
+++ src/app1.js
@@ -6,28 +6,41 @@ const path = require("path");
 
 const app= express();
 
-
+app.use(express.static("Frontend_folder"));
 app.use(express.json());
 
 let content = JSON.parse(fs.readFileSync("./data.json"))
-const userRouter = express.Router(); // express middleware
+// const userRouter = express.Router(); // express middleware
 const authRouter = express.Router(); // express middleware
 
-app.use('/user' , userRouter);
+// app.use('/user' , userRouter);
 app.use('/auth' , authRouter);
 
-userRouter.route('/').get(getUsers).post(bodyChecker , createUser);
 
-function createUser(req,res){
-  console.log("req.body", req.body);
 
-  let body = req.body;
-  console.log("req.body", req.body);
-  content.push(body);
-  fs.writeFileSync('./data.json', JSON.stringify(content));
-  res.json({message: content});
+authRouter.route('/signup').post(bodyChecker , signupUser);
 
-}
+
+
+
+
+
+
+// userRouter.route('/').get(getUsers).post(bodyChecker , createUser);
+
+
+
+
+// function createUser(req,res){
+//   console.log("req.body", req.body);
+
+//   let body = req.body;
+//   console.log("req.body", req.body);
+//   content.push(body);
+//   fs.writeFileSync('./data.json', JSON.stringify(content));
+//   res.json({message: content});
+
+// }
 
 function bodyChecker(req,res, next){
   let body = req.body;
@@ -40,36 +53,75 @@ function bodyChecker(req,res, next){
     }
     else{
 
-      res.send("Kind send details in body");
+      res.send("Kindly  send details in body");
     }
   }
 
-
-function getUsers(req, res) {
-    res.json({ message: content });
-}
-    app.listen(8081, function () {
-      console.log("server started");
-  })
+ function signupUser(req, res) // signup ki process
+ {
+   let {name , email , password , confirmPassword}=req.body; // request 
   
-  // app.post("/", function(req , res , next){
-  //   let body = req.body;
-  //   console.log("inside first post", body);
-  //   next();
-  // })
-  // app.use(function (req ,res , next){
-  //   console.log("inside app.use");
-  //   next();
-  // })
+   if(password == confirmPassword)
+   {
+     let newUser = {name , email , password};
+     content.push();
+     //saves data in data storage
+     fs.writeFileSync("data.json", JSON.stringify(content));
+     res.status(201).json({
+        cratedUser : newUser 
 
-  // app.get("/", function(req,res){
+     })
+     
+   }else{
+        res.status(422).json({
+            message: " Password and Confirm password do not match"
 
-  //   let body = req.body;
-  //   console.log("inside first get", body);
-  // })
-  // app.post("/" , function(req,res,next){
-  //   let body = req.body;
-  //   console.log('inside second post', body);
-  //   res.send('tested next');
-  // })
+        })
+
+   }
+
+ }
+
+
+// function getUsers(req, res) {
+//     res.json({ message: content });
+// }
+//     app.listen(8081, function () {
+//       console.log("server started");
+//   })
+  
+ app.listen(8081 , function(){
+
+    console.log("server started");
+
+ })
+
+
+  app.post("/", function(req , res , next){
+    let body = req.body;
+    console.log("inside first post", body);
+    next();
+  })
+  app.use(function (req ,res , next){
+    console.log("inside app.use");
+    next();
+  })
+
+  app.get("/", function(req,res){
+
+    let body = req.body;
+    console.log("inside first get", body);
+  })
+  app.post("/" , function(req,res,next){
+    let body = req.body;
+    console.log('inside second post', body);
+    res.send('tested next');
+  })
+
+  app.use(function (req, res){
+
+      res.status(404).sendFile(path.jpin(__dirname , "404.html"));
+  })
+
+  
   
